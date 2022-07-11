0db30a8a42917fae1e4d7d04acd5b65d77900931
Luiz Miyazaki <luiz.miyazaki@unico.io>
2022-07-11 13:50:21 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,136 @@
+import './App.css';
+import { UnicoCheckBuilder, SelfieCameraTypes, UnicoThemeBuilder, DocumentCameraTypes } from 'unico-webframe';
+// import { UnicoCheckBuilder, SelfieCameraTypes, UnicoThemeBuilder, DocumentCameraTypes } from 'unico-webframe';
+
+const callback = {
+  on: {
+    success: function(obj) {
+      console.log(obj.base64);
+    },
+    error: function(error) {
+      console.error(error)
+      //confira na aba "Configurações" sobre os tipos de erros
+    }
+  }
+};
+
+const CameraSDK = () => {
+
+  const unicoTheme = new UnicoThemeBuilder()
+    .setColorSilhouetteSuccess("#0384fc")
+    .setColorSilhouetteError("#D50000")
+    .setColorSilhouetteNeutral("#fcfcfc")
+    .setBackgroundColor("#dff1f5")
+    .setColorText("#0384fc")
+    .setBackgroundColorComponents("#0384fc")
+    .setColorTextComponents("#dff1f5")
+    .setBackgroundColorButtons("#0384fc")
+    .setColorTextButtons("#dff1f5")
+    .setBackgroundColorBoxMessage("#fff")
+    .setColorTextBoxMessage("#000")
+    .setHtmlPopupLoading(`<div style="position: absolute; top: 45%; right: 50%; transform:
+    translate(50%, -50%); z-index: 10; text-align: center;">Carregando...</div>`)
+    .build();
+
+  const unicoCamera = new UnicoCheckBuilder()
+    .setResourceDirectory("/resources")
+    .setModelsPath("/models")
+    .setTheme(unicoTheme)
+    .build();
+
+  // const cameraPromised = unicoCamera.prepareSelfieCamera("/services.json", SelfieCameraTypes.NORMAL);
+
+  // cameraPromised.then(cameraOpener => cameraOpener.open(callback));
+
+  const openSelfieCameraNormal = async () => {
+    const cameraPromised = unicoCamera
+      .prepareSelfieCamera("/web_s_ft_service.json", SelfieCameraTypes.NORMAL)
+      .catch(()=>console.error('Error initializing normal camera'));;
+    
+    cameraPromised.then(cameraOpener => cameraOpener.open(callback));
+  }
+  
+  const openSelfieCameraSmart = async () => {
+    const cameraPromised = unicoCamera
+      .prepareSelfieCamera("/web_s_ft_service.json", SelfieCameraTypes.SMART)
+      .catch(()=>console.error('Error initializing smart camera'));
+    
+    cameraPromised.then(cameraOpener => cameraOpener.open(callback));
+  }
+
+  const openSelfieCameraLiveness = async () => {
+    const cameraPromised = unicoCamera
+      .prepareSelfieCamera("/web_c_ft_service.json", SelfieCameraTypes.SMART)
+      // .catch(()=>console.error('Error initializing liveness camera'));
+    
+    cameraPromised.then(cameraOpener => cameraOpener.open(callback));
+  }
+
+  const openDocumentCameraCNH = async () => {
+    const cameraPromised = unicoCamera
+      .prepareDocumentCamera("/web_c_ft_service.json", DocumentCameraTypes.CNH)
+      .catch(()=>console.error('Error initializing CNH camera'));
+    
+    cameraPromised.then(cameraOpener => cameraOpener.open(callback));
+  }
+
+  const openDocumentCameraOutros = async () => {
+    const cameraPromised = unicoCamera
+      .prepareDocumentCamera("/web_c_ft_service.json", DocumentCameraTypes.OTHERS("Teste"))
+      .catch(()=>console.error('Error initializing other documents camera'));
+    
+    cameraPromised.then(cameraOpener => cameraOpener.open(callback));
+  }
+
+  return (
+    <>
+      {/* <div id="box-camera">
+        Sem FaceTech
+      </div> */}
+      <button
+        type="button"
+        onClick={() => openSelfieCameraNormal()}
+      >
+        Camera Normal
+      </button>
+      <button
+        type="button"
+        onClick={() => openSelfieCameraSmart()}
+      >
+        Camera Smart
+      </button>
+      <button
+        type="button"
+        onClick={() => openSelfieCameraLiveness()}
+      >
+        Camera Liveness
+      </button>
+      <button
+        type="button"
+        onClick={() => openDocumentCameraCNH()}
+      >
+        Camera CNH
+      </button>
+      <button
+        type="button"
+        onClick={() => openDocumentCameraOutros()}
+      >
+        Camera Outros
+      </button>
+      <div className="container">
+        <div id="box-camera">
+        </div>
+      </div>
+    </>
+  );
+};
+
+function App() {
+  return (
+    <div className="App">
+      <CameraSDK />
+    </div>
+  );
+}
+
+export default App;
